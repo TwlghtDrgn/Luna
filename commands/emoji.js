72659b023164dfb7abcb0e1849f97ad95bdf5962
Twilight Dragon <55512066+TwlghtDrgn@users.noreply.github.com>
2022-09-20 09:49:26 +0300

    Emoji test

--- commands/emoji.js
+++ commands/emoji.js
@@ -3,8 +3,25 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('emoji')
-        .setDescription('Send emoji that known to me'),
+        .setDescription('Send emoji that known to me')
+        .addStringOption(option =>
+            option.setName('emoji')
+                .setDescription('Select emoji to send')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'Shrugging Derpy', value: 'emoji_derp' },
+                    { name: 'Boulder', value: 'emoji_buldiga' },
+                )),
     async execute(interaction) {
-        await interaction.reply('Still not implemented yet <:derpyShrug_1:711863356415541269>');
+        const name = interaction.options.getString('emoji');
+        let resp = 'No emoji';
+
+        if (name === 'emoji_buldiga') {
+            resp = '<:pwBuldiga:630872804505681931>';
+        } else if (name === 'emoji_derp') {
+            resp = '<:derpyShrug_0:643766689376501760>';
+        }
+
+        await interaction.reply(resp);
     },
 };
