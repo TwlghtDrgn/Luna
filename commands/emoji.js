3eb9925ae2dadffa8048db0fb6966a837bfcd259
Twilight Dragon <55512066+TwlghtDrgn@users.noreply.github.com>
2022-09-20 16:25:35 +0300

    Fixed emojis... again

--- commands/emoji.js
+++ commands/emoji.js
@@ -5,9 +5,9 @@ module.exports = {
         .setName('emoji')
         .setDescription('Send emoji that known to me')
         .addStringOption(option =>
-            option.setName('twlghtdrgn')
-                .setDescription('TwlghtDrgn\'s emojis')
-                .setRequired(false)
+            option.setName('emoji')
+                .setDescription('Global emojis')
+                .setRequired(true)
                 .addChoices(
                     { name: 'Shrugging Derpy', value: 'emoji_derp' },
                     { name: 'Shrugging Luna', value: 'emoji_lulu' },
@@ -21,67 +21,72 @@ module.exports = {
                     { name: 'FullHD 1080P', value: 'emoji_fhd' },
                     { name: 'UltraHD 4K', value: 'emoji_uhd' },
                     { name: 'Dead Chat', value: 'emoji_deadchat' },
-                ))
-        .addStringOption(option =>
-            option.setName('puffers')
-                .setDescription('PuffersWorld emojis')
-                .setRequired(false)
-                .addChoices(
+
                     { name: 'Boulder', value: 'emoji_buldiga' },
                 )),
     async execute(interaction) {
         const name = interaction.options.getString('emoji');
-        let resp = null;
+        let resp;
 
         switch (name) {
         // TwlghtDrgn's emotes
         case 'emoji_derp':
             resp = '<:derpyShrug_0:643766689376501760>';
-            return;
+            await interaction.reply(resp);
+            break;
         case 'emoji_lulu':
             resp = '<:luluShrug:711862558826692660>';
-            return;
+            await interaction.reply(resp);
+            break;
         case 'emoji_tia':
             resp = '<:tiaShrug:643786229548056576>';
-            return;
+            await interaction.reply(resp);
+            break;
         case 'emoji_repel':
             resp = '<:repel:911605102383595540>';
-            return;
+            await interaction.reply(resp);
+            break;
         case 'emoji_lyra':
             resp = '<:lyraThinking:860056938289430538>';
-            return;
+            await interaction.reply(resp);
+            break;
         case 'emoji_sus':
             resp = '<:sus:925809579558830140>';
-            return;
+            await interaction.reply(resp);
+            break;
         case 'emoji_facehoof':
             resp = '<:twiFacehoof:643785797723488256>';
-            return;
+            await interaction.reply(resp);
+            break;
         case 'emoji_uno':
             resp = '<:reverse:685486822104891481>';
-            return;
+            await interaction.reply(resp);
+            break;
         case 'emoji_fbi':
             resp = '<:fbi:932325031718047864>';
-            return;
+            await interaction.reply(resp);
+            break;
         case 'emoji_fhd':
             resp = '<:fhd:951388464706891786>';
-            return;
+            await interaction.reply(resp);
+            break;
         case 'emoji_uhd':
             resp = '<:uhd:951388856073211924>';
-            return;
+            await interaction.reply(resp);
+            break;
         case 'emoji_deadchat':
             resp = '<:deadchat:686572384819478531>';
-            return;
+            await interaction.reply(resp);
+            break;
 
         // PW Emotes
         case 'emoji_buldiga':
             resp = '<:pwBuldiga:630872804505681931>';
-            return;
-        }
-
-        if (resp != null) {
             await interaction.reply(resp);
-        } else {
-            await interaction.reply({ content: 'Emoji is not selected. Use one of the categories to select emoji', ephemeral: true });
+            break;
+
+        default:
+            await interaction.reply({ content: 'Emoji is not selected or does not exist. Use one of the categories to select emoji', ephemeral: true });
         }
     },
 };
