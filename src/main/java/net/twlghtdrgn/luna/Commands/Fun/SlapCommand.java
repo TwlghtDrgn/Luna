9d6ceacb696da8aec68979401e92ea1b6956194f
TwlghtDrgn <55512066+TwlghtDrgn@users.noreply.github.com>
2022-10-18 21:31:35 +0300

    SlapCommand now is 20% cooler

--- src/main/java/net/twlghtdrgn/luna/Commands/Fun/SlapCommand.java
+++ src/main/java/net/twlghtdrgn/luna/Commands/Fun/SlapCommand.java
@@ -1,6 +1,5 @@
 package net.twlghtdrgn.luna.Commands.Fun;
 
-import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.twlghtdrgn.luna.Luna;
@@ -18,21 +17,30 @@ public class SlapCommand extends ListenerAdapter {
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
         if (!event.getName().equals("slap")) return;
-        String pinger = event.getUser().getAsMention();
+        String pinger = event.getUser().getName();
         String pinged = event.getOption("user").getAsUser().getAsMention();
 
         ArrayList<String> resp = new ArrayList<>(Arrays.asList(
-                " взял кирпич и швырнул его в ",
-                " устал ждать прихода ",
-                " упустил свой шанс поймать ",
-                " потерял брелок, подаренный ему от ",
-                " пытается убежать от ",
-                " пошел искать ",
-                " выкрикнул имя "
+                "%s взял кирпич и швырнул его в %r",
+                "%s устал ждать прибытия %r и пришел к нему во двор",
+                "%s пытается свой шанс поймать %r за хвост",
+                "%s потерял брелок, который ему подарил %r",
+                "%r пытается убежать от %s",
+                "%s пошел искать %r",
+                "%s выкрикнул имя %r",
+                "%s не дождался %r и пошел к нему домой",
+                "%s стучится в дверь квартиры %r, ожидая ответа",
+                "%s пришел в комнату к %r, тихонько включил компьютер и начал играть в Minecraft с читами. Стоп, что?!",
+                "%s напомнил %r про давнее обещание",
+                "%s швырнул тапком в %r",
+                "%s кинул вонючие носки в сумку к %r и убежал, ехидно хихикая",
+                "%s пытается разбудить %r в три часа ночи",
+                "Злой и сонный %r угрожая тапком несется за %s по улице, ибо нефиг будить посреди ночи",
+                "%s подкрался сзади и резко крикнул %r на ухо какую-то неразборчивую хрень"
         ));
 
         int rand = (int) Math.floor(Math.random() * resp.size());
 
-        event.reply(pinger + resp.get(rand) + pinged).queue();
+        event.reply(resp.get(rand).replace("%s",pinger).replace("%r",pinged)).queue();
     }
 }
