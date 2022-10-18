041afccdf4e4e85f3553711c57752ae243e6b9a5
TwlghtDrgn <marakulin6260@gmail.com>
2022-10-18 13:16:57 +0300

    New commands

--- src/main/java/net/twlghtdrgn/luna/Commands/CommandManager.java
+++ src/main/java/net/twlghtdrgn/luna/Commands/CommandManager.java
@@ -1,6 +1,9 @@
 package net.twlghtdrgn.luna.Commands;
 
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.twlghtdrgn.luna.Commands.Fun.DiceCommand;
+import net.twlghtdrgn.luna.Commands.Fun.SlapCommand;
 import net.twlghtdrgn.luna.Commands.Media.RadioCommand;
 import net.twlghtdrgn.luna.Embeds.Embeds;
 import net.twlghtdrgn.luna.Luna;
@@ -19,12 +22,16 @@ public class CommandManager extends ListenerAdapter {
         this.luna = luna;
 
         luna.getShardManager().addEventListener(new RadioCommand(this.luna));
+        luna.getShardManager().addEventListener(new DiceCommand(this.luna));
+        luna.getShardManager().addEventListener(new SlapCommand(this.luna));
     }
 
     @Override
     public void onReady(ReadyEvent event) {
         List<CommandData> commandData = new ArrayList<>();
         commandData.add(Commands.slash("radio","Starts a radio in your Staged channel"));
+        commandData.add(Commands.slash("dice","Draw a number from 1 to 6"));
+        commandData.add(Commands.slash("slap","Slap someone (inspired by Cadyr)").addOption(OptionType.USER, "user","Person who you want to slap", true));
         event.getJDA().updateCommands().addCommands(commandData).queue();
         luna.getLogger().info("Commands updated successfully!");
     }
