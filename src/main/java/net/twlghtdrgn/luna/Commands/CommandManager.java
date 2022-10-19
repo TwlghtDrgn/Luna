24b581784c66b950000d70761f2426b5a7a2bbdf
TwlghtDrgn <55512066+TwlghtDrgn@users.noreply.github.com>
2022-10-19 20:37:42 +0300

    Emoji command is now working

--- src/main/java/net/twlghtdrgn/luna/Commands/CommandManager.java
+++ src/main/java/net/twlghtdrgn/luna/Commands/CommandManager.java
@@ -2,9 +2,9 @@ package net.twlghtdrgn.luna.Commands;
 
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
-//import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.twlghtdrgn.luna.Commands.Fun.DiceCommand;
-//import net.twlghtdrgn.luna.Commands.Fun.EmojiCommand;
+import net.twlghtdrgn.luna.Commands.Fun.EmojiCommand;
 import net.twlghtdrgn.luna.Commands.Fun.SlapCommand;
 import net.twlghtdrgn.luna.Commands.Media.RadioCommand;
 import net.twlghtdrgn.luna.Luna;
@@ -24,7 +24,7 @@ public class CommandManager extends ListenerAdapter {
         luna.getShardManager().addEventListener(new RadioCommand(this.luna));
         luna.getShardManager().addEventListener(new DiceCommand(this.luna));
         luna.getShardManager().addEventListener(new SlapCommand(this.luna));
-//        luna.getShardManager().addEventListener(new EmojiCommand(this.luna));
+        luna.getShardManager().addEventListener(new EmojiCommand(this.luna));
     }
 
     @Override
@@ -34,25 +34,25 @@ public class CommandManager extends ListenerAdapter {
         commandData.add(Commands.slash("dice","Draw a number from 1 to 6"));
         commandData.add(Commands.slash("slap","Slap someone (inspired by Cadyr, https://udj.at/cadyr)")
                 .addOption(OptionType.USER, "user","Person who you want to slap", true));
-// TODO: fix emoji command
-//        commandData.add(Commands.slash("emoji","Send emoji known to me")
-//                .addOptions(
-//                    new OptionData(OptionType.STRING,"twlghtdrgn","TwlghtDrgn's emojis",false)
-//                            .addChoice("Shrugging Derpy","emoji_derp")
-//                            .addChoice("Shrugging Luna","emoji_lulu")
-//                            .addChoice("Repel","emoji_repel")
-//                            .addChoice("Thinking Lyra","emoji_lyra")
-//                            .addChoice("SUS","emoji_sus")
-//                            .addChoice("Facehoof","emoji_facehoof")
-//                            .addChoice("Uno Reverse Card","emoji_uno")
-//                            .addChoice("Dad that watching your browser history","emoji_fbi")
-//                            .addChoice("FullHD 1080P","emoji_fhd")
-//                            .addChoice("UltraHD 4K","emoji_uhd")
-//                            .addChoice("Dead Chat","emoji_deadchat"),
-//                    new OptionData(OptionType.STRING,"puffers","PuffersWorld emojis",false)
-//                            .addChoice("Boulder", "boulder")
-//                )
-//        );
+        commandData.add(Commands.slash("emoji","Send emoji known to me")
+                .addOptions(
+                    new OptionData(OptionType.STRING,"twlghtdrgn","TwlghtDrgn's emojis",false)
+                            .addChoice("Shrugging Derpy","emoji_derp")
+                            .addChoice("Shrugging Luna","emoji_lulu")
+                            .addChoice("Shrugging Celestia","emoji_tia")
+                            .addChoice("Repel","emoji_repel")
+                            .addChoice("Thinking Lyra","emoji_lyra")
+                            .addChoice("SUS","emoji_sus")
+                            .addChoice("Facehoof","emoji_facehoof")
+                            .addChoice("Uno Reverse Card","emoji_uno")
+                            .addChoice("Dad who watches your browser history","emoji_fbi")
+                            .addChoice("FullHD 1080P","emoji_fhd")
+                            .addChoice("UltraHD 4K","emoji_uhd")
+                            .addChoice("Dead Chat","emoji_deadchat"),
+                    new OptionData(OptionType.STRING,"puffers","PuffersWorld emojis",false)
+                            .addChoice("Boulder", "emoji_buldiga")
+                )
+        );
         event.getJDA().updateCommands().addCommands(commandData).queue();
         luna.getLogger().info("Commands updated successfully!");
     }
--- src/main/java/net/twlghtdrgn/luna/Commands/Fun/EmojiCommand.java
+++ src/main/java/net/twlghtdrgn/luna/Commands/Fun/EmojiCommand.java
@@ -14,29 +14,38 @@ public class EmojiCommand extends ListenerAdapter {
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
         if (!event.getName().equals("emoji")) return;
-        // TODO: Find a way to use options without getting an error
-        Object twlght = event.getOption("twlghtdrgn");
-        Object puff = event.getOption("puffers");
+        String twlght = "null", puff = "null", val = "null";
 
-        if (twlght != null) {
-            String val = twlght.toString();
-            switch (val) {
-                case ("emoji_derp") -> {
-                    event.reply("<:derpyShrug_0:643766689376501760>").queue();
-                }
+        if (event.getOption("twlghtdrgn") != null) twlght = event.getOption("twlghtdrgn").getAsString();
+        if (event.getOption("puffers") != null) puff = event.getOption("puffers").getAsString();
+
+        if (!twlght.equals("null")) {
+            switch (twlght) {
+                case ("emoji_derp") -> val = "<:derpyShrug_0:643766689376501760>";
+                case ("emoji_lulu") -> val = "<:luluShrug:711862558826692660>";
+                case ("emoji_tia") -> val = "<:tiaShrug:643786229548056576>";
+                case ("emoji_repel") -> val = "<:repel:911605102383595540>";
+                case ("emoji_lyra") -> val = "<:lyraThinking:860056938289430538>";
+                case ("emoji_sus") -> val = "<:sus:925809579558830140>";
+                case ("emoji_facehoof") -> val = "<:twiFacehoof:643785797723488256>";
+                case ("emoji_uno") -> val = "<:reverse:685486822104891481>";
+                case ("emoji_fbi") -> val = "<:fbi:932325031718047864>";
+                case ("emoji_fhd") -> val = "<:fhd:951388464706891786>";
+                case ("emoji_uhd") -> val = "<:uhd:951388856073211924>";
+                case ("emoji_deadchat") -> val = "<:deadchat:686572384819478531>";
             }
-
+            event.reply(val).queue();
             return;
         }
-        if (puff != null) {
-            String val = puff.toString();
-            switch (val) {
-                case ("emoji_buldiga") -> {
-                    event.reply("<:pwBuldiga:630872804505681931>").queue();
-                }
+
+        if (!puff.equals("null")) {
+            switch (puff) {
+                case ("emoji_buldiga") -> val = "<:pwBuldiga:630872804505681931>";
             }
+            event.reply(val).queue();
             return;
         }
+
         event.reply("Emoji is not selected or does not exist. Use one of the categories to select emoji")
                 .setEphemeral(true).queue();
     }
