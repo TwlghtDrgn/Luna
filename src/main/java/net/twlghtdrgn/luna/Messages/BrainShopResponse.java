8f26738a28267a9bce859d07118128c332c06436
TwlghtDrgn <55512066+TwlghtDrgn@users.noreply.github.com>
2022-10-24 11:16:01 +0300

    renamed from MessageResponse to BrainShopResponse

--- src/main/java/net/twlghtdrgn/luna/Messages/BrainShopResponse.java
+++ src/main/java/net/twlghtdrgn/luna/Messages/BrainShopResponse.java
@@ -5,17 +5,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import net.twlghtdrgn.luna.Luna;
+import net.twlghtdrgn.luna.Utils.JSONParser;
 
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
 
-public class MessageResponse {
+public class BrainShopResponse {
     private final Luna luna;
 
-    public MessageResponse(Luna luna) {
+    public BrainShopResponse(Luna luna) {
         this.luna = luna;
     }
 
@@ -31,33 +30,15 @@ public class MessageResponse {
                     + "&uid=" + URLEncoder.encode(uid, StandardCharsets.UTF_8)
                     + "&msg=" + URLEncoder.encode(msg, StandardCharsets.UTF_8));
 
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.connect();
-
-            int resp = conn.getResponseCode();
-
-            if (resp != 200) {
-                throw new RuntimeException("HttpRespCode: " + resp);
-            }
-
-            String jsResp = "";
-            Scanner scanner = new Scanner(url.openStream());
-
-            while (scanner.hasNext()) {
-                jsResp += scanner.nextLine();
-            }
-
-            scanner.close();
+            String jsResp = new JSONParser(luna).JSON(url);
 
             ObjectMapper mapper = new ObjectMapper();
             message response = mapper.readValue(jsResp.getBytes(), message.class);
 
             return response.getCnt().replace(".U003C","<").replace(".U003E",">");
         } catch (Exception e) {
-//           luna.getLogger().error(e.toString());
-            e.printStackTrace();
-            return "Произошла ошибка (но прикладывать я ее не буду). Напиши в ЛС <@339488218523238410>, приложив это сообщение";
+           luna.getLogger().error(e.toString());
+           return "Произошла ошибка (но прикладывать я ее не буду). Напиши в ЛС <@339488218523238410>, приложив это сообщение";
         }
     }
 
