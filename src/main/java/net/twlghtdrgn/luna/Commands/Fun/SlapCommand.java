bffdd0ecf2c8911acbe7d65ba11171d330ab8d5a
TwlghtDrgn <55512066+TwlghtDrgn@users.noreply.github.com>
2022-10-24 11:13:35 +0300

    Updated SlapCommand

--- src/main/java/net/twlghtdrgn/luna/Commands/Fun/SlapCommand.java
+++ src/main/java/net/twlghtdrgn/luna/Commands/Fun/SlapCommand.java
@@ -16,14 +16,41 @@ public class SlapCommand extends ListenerAdapter {
 
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        if (!event.isFromGuild()) return;
         if (!event.getName().equals("slap")) return;
-        String pinger = event.getUser().getName();
+        String pinger = event.getUser().getAsMention();
         String pinged = event.getOption("user").getAsUser().getAsMention();
+        Boolean isMegaSlap = false;
 
+        if (event.getOption("ismega") != null) isMegaSlap = event.getOption("ismega").getAsBoolean();
+
+        ArrayList<String> resp;
+        if (pinged.equals(event.getJDA().getSelfUser().getAsMention())) {
+            resp = easterEggResponses();
+        } else resp = normalResponses();
+
+        if (isMegaSlap) {
+            event.reply("Executed MegaSlap").setEphemeral(true).queue();
+            int i = 0;
+            String msg = "";
+            while (i <= 5){
+                int rand = (int) Math.floor(Math.random() * resp.size());
+                msg += resp.get(rand)+"\n";
+                i++;
+            }
+            event.getChannel().asTextChannel().sendMessage(msg.replace("%s", pinger).replace("%r", pinged)).queue();
+        } else {
+            int rand = (int) Math.floor(Math.random() * resp.size());
+            event.reply(resp.get(rand).replace("%s", pinger).replace("%r", pinged)).queue();
+        }
+    }
+
+    public ArrayList<String> normalResponses() {
         ArrayList<String> resp = new ArrayList<>(Arrays.asList(
+                "%s заорал вслед %r 'КУПИ СЛОНА!!!111'",
                 "%s взял кирпич и швырнул его в %r",
                 "%s устал ждать прибытия %r и пришел к нему во двор",
-                "%s пытается свой шанс поймать %r за хвост",
+                "%s старается поймать %r за хвост",
                 "%s потерял брелок, который ему подарил %r",
                 "%r пытается убежать от %s",
                 "%s пошел искать %r",
@@ -39,8 +66,18 @@ public class SlapCommand extends ListenerAdapter {
                 "%s подкрался сзади и резко крикнул %r на ухо какую-то неразборчивую хрень"
         ));
 
-        int rand = (int) Math.floor(Math.random() * resp.size());
+        return resp;
+    }
 
-        event.reply(resp.get(rand).replace("%s",pinger).replace("%r",pinged)).queue();
+    public ArrayList<String> easterEggResponses() {
+        ArrayList<String> resp = new ArrayList<>(Arrays.asList(
+                "%r теперь гонится за %s, так как этот гений подкрался к ней сзади и напугал её",
+                "Знаете, я испытываю ну оочень сильное желание сослать %s на Солнце. Просто по приколу =)",
+                "%r продала компьютер %s за его назойливость",
+                "%r подозревает %s в том, что он использует какие-то вещи ну оооочень не по назначению",
+                "\\**звуки начала боя покемонов*\\*\n%r - 99999hp \t%s - 852hp"
+        ));
+
+        return resp;
     }
 }
