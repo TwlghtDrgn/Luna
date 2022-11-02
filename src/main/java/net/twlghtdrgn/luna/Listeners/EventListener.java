886afb0a009813cedb8cc638625f0e520c13450f
TwlghtDrgn <55512066+TwlghtDrgn@users.noreply.github.com>
2022-11-03 00:10:13 +0300

    Added shard info into log

--- src/main/java/net/twlghtdrgn/luna/Listeners/EventListener.java
+++ src/main/java/net/twlghtdrgn/luna/Listeners/EventListener.java
@@ -1,12 +1,9 @@
 package net.twlghtdrgn.luna.Listeners;
 
-import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.twlghtdrgn.luna.Luna;
-import org.jetbrains.annotations.NotNull;
 
 public class EventListener extends ListenerAdapter {
     private final Luna luna;
@@ -17,6 +14,7 @@ public class EventListener extends ListenerAdapter {
 
     @Override
     public void onReady(ReadyEvent event) {
+        luna.getLogger().info("Using " + event.getJDA().getShardInfo().getShardTotal()  + " shard(s), found " + event.getJDA().getGuilds().size() + " guild(s)");
         luna.getLogger().info("Initialization complete! Logged in as " + event.getJDA().getSelfUser().getAsTag());
     }
 
